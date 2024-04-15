45d7bd56429c08e0c8e35e874ac721564783b63c
Eivind Strand Harboe <eiha0403@gmail.com>
2024-04-15 12:56:35 +0200

    Some additions to booth

--- booth.py
+++ booth.py
@@ -1,21 +1,31 @@
 from stmpy import Driver, Machine
 from threading import Thread
-import json 
+import json
 
 import paho.mqtt.client as mqtt
 from env import STATION_TOPIC, BOOTH_TOPIC, BROKER, PORT
 
 
 class Booth:
-    def im_occupied(self, id):
-        print("im_occupied triggered! id: " +id)
-        self.mqtt_client.publish(BOOTH_TOPIC, {"msg": "occupied", "id": id})
-    def im_down(self, id):
-        print("im_down triggered!")
-        self.mqtt_client.publish(BOOTH_TOPIC, {"msg": "down", "id": id})
-    def im_ready(self, id):
-        print("im_ready triggered!")
-        self.mqtt_client.publish(BOOTH_TOPIC, {"msg": "ready", "id": id})
+
+    def send(self, args):
+        id = args[0]
+        msg = args[1]
+        match msg:
+            case "ready":
+                print("im_ready triggered!")
+                self.mqtt_client.publish(BOOTH_TOPIC, {"msg": "ready", "id": id})
+            case "occupied":
+                print("im_occupied triggered! id: " + id)
+                self.mqtt_client.publish(BOOTH_TOPIC, {"msg": "occupied", "id": id})
+            case "down":
+                print("im_down triggered!")
+                self.mqtt_client.publish(BOOTH_TOPIC, {"msg": "down", "id": id})
+
+    def register(self, args):
+        print("register triggered!")
+        self.mqtt_client.publish(BOOTH_TOPIC, {"msg": "register", "id": args[0]})
+
 
 # initial transition
 t0 = {"source": "initial", "target": "operative", "effect": "register(*)"}
@@ -24,28 +34,24 @@ t1 = {
     "trigger": "occupied",
     "source": "operative",
     "target": "operative",
-    "effect": "im_occupied(*)"
+    "effect": "send(*)",
 }
 
 t2 = {
     "trigger": "down",
     "source": "operative",
     "target": "operative",
-    "effect" : "im_down(*)"
+    "effect": "send(*)",
 }
 
 t3 = {
     "trigger": "ready",
     "source": "operative",
     "target": "operative",
-    "effect" : "im_ready(*)"
+    "effect": "sendf*)",
 }
 
-t4 = {
-    "trigger": "sys_err",
-    "source": "operative",
-    "target": "out_of_order"
-}
+t4 = {"trigger": "sys_err", "source": "operative", "target": "out_of_order"}
 
 s = {
     "name": "operative",
@@ -54,9 +60,6 @@ s = {
 s1 = {
     "name": "out_of_order",
 }
-
-
-
 
 
 class MQTT_Client_1:
@@ -70,25 +73,19 @@ class MQTT_Client_1:
         print("on_connect(): {}".format(mqtt.connack_string(rc)))
 
     def on_message(self, client, userdata, msg):
-        
+
         payload = json.loads(msg.payload)
 
         print("on_message(): topic: {}, message: {}".format(msg.topic, payload["msg"]))
-        if(payload["msg"] != "booth" and payload["msg"] != "occupied" and payload["msg"] != "down" and payload["msg"] != "ready"):
+        if (
+            payload["msg"] != "booth"
+            and payload["msg"] != "occupied"
+            and payload["msg"] != "down"
+            and payload["msg"] != "ready"
+        ):
             print(payload["msg"] + " is not a valid message ignoring")
-        
         else:
-
-            if(payload["msg"] == "occupied"):
-                self.stm_driver.send("occupied", "booth", args=[payload["id"]])
-            elif(payload["msg"] == "down"):
-                self.stm_driver.send("down", "booth", args=[payload["id"]])
-            elif(payload["msg"] == "ready"):
-                self.stm_driver.send("ready", "booth", args=[payload["id"]])
-            else:
-                print("This should not happen")
-        #self.stm_driver.send(payload["msg"],"buzzer")
-
+            self.stm_driver.send("send", "booth", args=[payload["id"], payload["msg"]])
 
     def start(self, broker, port):
 
@@ -107,7 +104,9 @@ class MQTT_Client_1:
 
 
 booth = Booth()
-booth_machine = Machine(transitions=[t0, t1, t2, t3, t4], states=[s, s1], obj=booth, name="booth")
+booth_machine = Machine(
+    transitions=[t0, t1, t2, t3, t4], states=[s, s1], obj=booth, name="booth"
+)
 booth.stm = booth_machine
 
 driver = Driver()
